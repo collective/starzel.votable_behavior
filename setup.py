2cb8ab3793e265043d31703b22ccea90e1529f74
ksuess <k.suess@rohberg.ch>
2022-07-15 10:27:55 +0200

    Remove invalid extra 'blobs' of 'plone.namedfile [blobs]'. Remove not needed

--- setup.py
+++ setup.py
@@ -1,41 +1,40 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0'
+version = "1.0"
 
-setup(name='starzel.votable_behavior',
-      version=version,
-      description="Voting Behavior",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name="starzel.votable_behavior",
+    version=version,
+    description="Voting Behavior",
+    long_description=open("README.rst").read()
+    + "\n"
+    + open(os.path.join("docs", "HISTORY.txt")).read(),
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='',
-      author='',
-      author_email='',
-      url='https://github.com/collective/starzel.votable_behavior',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['starzel'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'plone.app.dexterity',
-          'plone.app.relationfield',
-          'plone.namedfile [blobs]',
-          'z3c.autoinclude',
-
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
+    ],
+    keywords="",
+    author="",
+    author_email="",
+    url="https://github.com/collective/starzel.votable_behavior",
+    license="GPL",
+    packages=find_packages(exclude=["ez_setup"]),
+    namespace_packages=["starzel"],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "setuptools",
+        "plone.app.dexterity",
+        "z3c.autoinclude",
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      )
+)
